dbb27677fb03b2f8631a7e26a4e7f676ccb089c2
Colin Flaherty <colinflaherty512@gmail.com>
2019-02-22 04:09:59 -0600

    rought_draft_landing_page

--- website/src/main.js
+++ website/src/main.js
@@ -6,10 +6,13 @@ import router from "./router";
 import store from "./store";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Ripple from 'vue-ripple-directive'
 
 Vue.config.productionTip = false;
 
 Vue.use(bootstrap)
+Vue.directive('ripple', Ripple);
+
 
 new Vue({
   router,
--- website/src/views/Home.vue
+++ website/src/views/Home.vue
@@ -1,50 +1,104 @@
 <template>
-  <b-container class="home px-0" fluid>
-		
+
+  <b-container class="home px-0" fluid>	
 		<!-- Nav bar -->
-		<b-navbar id="navbar" class="py-3">
-			<b-navbar-brand id="title" href="#" class="float-left ml-5 font-italic">Scoop</b-navbar-brand>
+		<b-navbar id="navbar" class="d-flex">
+			<b-navbar-brand id="title" href="#" class="px-5 mr-auto">Scoop</b-navbar-brand>
+			<b-nav-text class="nav_link px-5"><router-link tag="span" id="nav_link" to="/">Login</router-link></b-nav-text>
 		</b-navbar>
-	<b-row>
-		<b-col>
-		<b-carousel
-    id="carousel-fade"
-    style="text-shadow: 0px 0px 2px #000; z-index:0"
-    fade
-		interval="5"
-    img-width="1024"
-    img-height="480"
-  >
-    <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=10" />
-    <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=12" />
-    <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=22" />
-			<b-container class="home px-0" fluid>
-					<b-row>
-						<b-col cols="1"/>
-						<b-col>
-							<b-card
-    						title="Card Title"
-    						img-src="https://picsum.photos/600/300/?image=25"
-    						img-alt="Image"
-    						img-top
-    						tag="article"
-    						style="max-width: 20rem; z-index: 1;"
-    						class="mb-2"
-  						>
-   	 						<b-card-text>
-      						Some quick example text to build on the card title and make up the bulk of the card's content.
-    						</b-card-text>
 
-    						<b-button href="#" variant="primary">Go somewhere</b-button>
-  						</b-card>
-						</b-col>
-					</b-row>
-			</b-container>
-		</b-carousel>
+		<b-row class="invisible-md"/>
 
+		<b-row>
+			<b-col cols="1"/>
+			<b-col cols="4">	
+				<b-card
+    			style="max-width: 20rem; width: 40vw; min-width: 559px;"
+    			class="mb-2 shadow-lg"
+ 				>
 
-		</b-col>
-	</b-row>
+					<div class="my-4" id="lead">
+						Your optimized online ad campaign is just a few clicks away...	
+					</div>
+
+					<div class="border-bottom pb-4 mb-4 text-left">
+						Scoop takes your existing paper ad material, gives you feedback on it, and then 
+						generates a complete, optimized marketing campaign across Facebook, Instagram, Google,
+						and more.
+
+					</div>
+    			<b-form  @submit.prevent>
+
+						<b-input 
+							type="email" 
+							id="textEmail" 
+							placeholder="Enter email..."
+							class="mb-4"/>
+
+    				<b-input 
+							type="password" 
+							id="textPassword" 
+							aria-describedby="passwordHelpBlock"
+							placeholder="Enter password..."
+							class="mb-2"/>
+    				<b-form-text id="passwordHelpBlock" class="mb-4">
+      				Your password must be 8-20 characters long, contain letters and numbers, and must not
+      				contain spaces, special characters, or emoji.
+    				</b-form-text>
+
+						<b-button id="get_started" type="submit" v-ripple>Get started</b-button>
+
+   				</b-form>
+
+					<div style="font-size: 12px" class="pt-3">
+						Try Scoop free for 14 days. No risk, and no credit card required.
+					</div>
+
+  			</b-card>
+			</b-col>
+			<b-col cols="1"/>
+			<b-col cols="5 mx-5 text-left">
+					<div class="invisible-lg"/>
+
+					<div class="step pb-3" style="color: white">
+						<span class="num"> 1. </span>
+						Submit pictures of your paper ads.
+					</div>
+					<div class="step pb-3" style="color: white">
+						<span class="num"> 2. </span>
+						Tell us a little about your typical customer.
+						(Nothing too complicated. We promise.)
+					</div>
+					<div class="step pb-3">
+						<span class="num"> 3. </span>
+						Set your budget.			
+					</div>
+					<div class="step pb-3">
+						<span class="num"> 4. </span>
+						Sit back and let us handle the rest!						
+					</div>
+
+			</b-col>
+			<b-col cols="1"/>
+		</b-row>	
+
+		<b-row class="invisible-lg"/>
+		<b-row class="invisible-lg"/>
+
+		<b-row align-h="center">
+			<b-col cols="3">
+				
+				hi
+			</b-col>
+			<b-col cols="3">
+				hi
+			</b-col>
+			<b-col cols="3">
+				hi
+			</b-col>
+
+		</b-row>
+
 
 	</b-container>
 </template>
@@ -62,9 +116,42 @@ export default {
 </script>
 
 <style lang="stylus">
-#navbar
-	background-color #ff5757
+
+@import url("https://fonts.googleapis.com/css?family=Archivo")
+@import url("https://fonts.googleapis.com/css?family=Varela+Round")
+
+
 #title
-	font-size 32px
+	font-size 60px
+	font-weight bold
 	color white
+.nav_link
+	font-size 40px 
+	font-weight normal !important
+	color white !important
+	&:hover
+		cursor pointer
+.home
+	background url("../assets/hero_long.png") top center no-repeat
+	min-height 650px
+	font-family 'Varela Round', sans-serif
+	color #433E3F
+#lead
+	font-size 38px
+	font-weight bold
+	text-align left
+.invisible-md
+	min-height 25px
+.invisible-lg
+	min-height 50px
+#get_started
+	background-color #ec7373
+	border-color #ec7373
+	border-radius 20px
+.num
+	font-size 50px
+	font-weight 900
+.step
+	font-size 26px
+	font-weight bold
 </style>
